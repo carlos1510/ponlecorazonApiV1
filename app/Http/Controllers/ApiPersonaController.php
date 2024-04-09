bec31260e0314e599edb0a190d583b8bc327c589
carlos1510 <carloscle1510@gmail.com>
2024-04-09 00:04:24 -0500

    agrega modificacion en la consulta a dni persona

--- app/Http/Controllers/ApiPersonaController.php
+++ app/Http/Controllers/ApiPersonaController.php
@@ -8,7 +8,7 @@ use App\Models\Persona;
 class ApiPersonaController extends Controller
 {
     public function show($numero_documento, Request $request){
-        $persona  = Persona::where("numero_documento",$numero_documento)->first();
+        $persona  = Persona::select('id','numero_documento','apellido_paterno','apellido_matero','nombres','nombre','sexo','ubigero_reniec','verificado')->SelectRaw('DATE_FORMAT(fecha_nacimiento,"%d/%m/%Y") as fecha_nacimiento')->where("numero_documento",$numero_documento)->first();
 
         return response()->json($persona);
     }
